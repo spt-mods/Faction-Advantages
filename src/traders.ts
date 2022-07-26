6f379e9a3e56055ba7e2c627e78223cd8acf752d
citharus <citharus@protonmail.com>
2022-07-27 00:16:42 +0200

    Created `updateRepairPriceForFactionTraders` method

--- src/traders.ts
+++ src/traders.ts
@@ -1,10 +1,34 @@
 import {DependencyContainer} from "tsyringe";
 import {ILogger} from "@spt-aki/models/spt/utils/ILogger";
+import {ProfileHelper} from "@spt-aki/helpers/ProfileHelper";
+import {DatabaseServer} from "@spt-aki/servers/DatabaseServer";
+
+import traderConfig = require("../config/traders.json");
+import factionTraders = require("../database/traders.json");
 
 export class Traders {
     private logger: ILogger;
+    private profileHelper: ProfileHelper;
+    private databaseServer: DatabaseServer;
 
     constructor(container: DependencyContainer) {
         this.logger = container.resolve<ILogger>("WinstonLogger");
+        this.profileHelper = container.resolve<ProfileHelper>("ProfileHelper");
+        this.databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+    }
+
+    /**
+     * Update the repair price of traders of the player's faction with the multipliers from the trader config.
+     * @param sessionID The profile ID of the logged in player.
+     */
+    updateRepairPriceForFactionTraders(sessionID: string): void {
+        const playerFaction: string = this.profileHelper.getPmcProfile(sessionID).Info.Side;
+        const databaseTraders = this.databaseServer.getTables().traders;
+
+        for (const trader of factionTraders[playerFaction]) {
+            databaseTraders[trader].base.loyaltyLevels.map(loyaltyLevel => loyaltyLevel.repair_price_coef * Math.round(
+                traderConfig.RepairPriceMultiplier / 100
+            ));
+        }
     }
 }
