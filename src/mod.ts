41e16360c7c27ef96b29984b4abf5320b657440d
citharus <citharus@protonmail.com>
2022-07-26 16:14:21 +0200

    Created `StaticRouteUpdateWeaponStatsForFaction` hoock

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,30 @@
 import {DependencyContainer} from "tsyringe";
 import {IPostAkiLoadMod} from "@spt-aki/models/external/IPostAkiLoadMod";
+import {ILogger} from "@spt-aki/models/spt/utils/ILogger";
+import {StaticRouterModService} from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
+
+import {Weapons} from "./weapons";
+
 class Mod implements IPostAkiLoadMod {
     private package = require("../package.json");
 
     postAkiLoad(container: DependencyContainer): void {
         const logger = container.resolve<ILogger>("WinstonLogger");
+        const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
 
         logger.info(`Loading: ${this.package.displayName}`);
+
+        staticRouterModService.registerStaticRouter(
+            "StaticRouteUpdateWeaponStatsForFaction",
+            [{
+                url: "/client/game/start",
+                action: (url: string, info: any, sessionID: string, output: string) => {
+                    new Weapons(container).updateWeaponStatsForFaction(sessionID);
+                    return output;
+                }
+            }],
+            "aki"
+        );
     }
 }
 
