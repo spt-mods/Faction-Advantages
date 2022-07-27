889c4b39ed256aab98a6c041b03d400005fbebd5
citharus <citharus@protonmail.com>
2022-07-27 15:25:10 +0200

    Created `setTraderDiscountForFactionTraders` method

--- src/traders.ts
+++ src/traders.ts
@@ -2,6 +2,8 @@ import {DependencyContainer} from "tsyringe";
 import {ILogger} from "@spt-aki/models/spt/utils/ILogger";
 import {ProfileHelper} from "@spt-aki/helpers/ProfileHelper";
 import {DatabaseServer} from "@spt-aki/servers/DatabaseServer";
+import {PaymentHelper} from "@spt-aki/helpers/PaymentHelper";
+import {ITraderAssort} from "@spt-aki/models/eft/common/tables/ITrader";
 
 import traderConfig = require("../config/traders.json");
 import factionTraders = require("../database/traders.json");
@@ -10,11 +12,13 @@ export class Traders {
     private logger: ILogger;
     private profileHelper: ProfileHelper;
     private databaseServer: DatabaseServer;
+    private paymentHelper: PaymentHelper;
 
     constructor(container: DependencyContainer) {
         this.logger = container.resolve<ILogger>("WinstonLogger");
         this.profileHelper = container.resolve<ProfileHelper>("ProfileHelper");
         this.databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        this.paymentHelper = container.resolve<PaymentHelper>("PaymentHelper")
     }
 
     /**
@@ -29,6 +33,20 @@ export class Traders {
             databaseTraders[trader].base.loyaltyLevels.map(loyaltyLevel => loyaltyLevel.repair_price_coef = Math.round(
                 loyaltyLevel.repair_price_coef * traderConfig.RepairPriceMultiplier / 100
             ));
+        }
+    }
+
+    /**
+     * Set a discount for traders of the player's faction with the setting in the trader config.
+     * @param sessionID The profile ID of the logged in player.
+     */
+    setTraderDiscountForFactionTraders(sessionID: string): void {
+        const playerFaction: string = this.profileHelper.getPmcProfile(sessionID).Info.Side;
+        const databaseTraders = this.databaseServer.getTables().traders;
+
+        for (const trader of factionTraders[playerFaction]) {
+            this.applyDiscountToTraderAssort(databaseTraders[trader].assort);
+            databaseTraders[trader].base.discount = -traderConfig.TraderDiscount;
         }
     }
 
